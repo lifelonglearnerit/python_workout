4d81443b399add72793be41eb89881cd899c1786
lifelonglearnerit <lifelonglearnerit@gmail.com>
2022-02-23 08:49:37 +0100

    added sum_nummeric(*args)

--- exercise_10_summing_anything.py
+++ exercise_10_summing_anything.py
@@ -4,6 +4,8 @@ such that it can take any number of arguments. The arguments must all be of the
 same type and know how to respond to the + operator (Thus, the function should
 work with numbers, strings, lists, and tuples, but not sets and dicts)
 """
+
+
 # my solution
 def mysum(*args):
     if not args:
@@ -13,7 +15,9 @@ def mysum(*args):
         for i in args[1:]:
             result += i
         return result
-print(mysum([1,2,3,4,5], [50, 90], [0]))
+
+
+print(mysum([1, 2, 3, 4, 5], [50, 90], [0]))
 print(mysum())
 
 # Beyond the exercise:
@@ -26,6 +30,8 @@ work with any type and assumes that all of the arguments are of the same type. N
 > and < work on many different types in Python,not just on number; with strings, lists, and 
 tuples, it refers to their sort order.
 """
+
+
 def mysum_bigger_than(x, *args):
     print('1:', args)
     if not args:
@@ -37,5 +43,28 @@ def mysum_bigger_than(x, *args):
             result += i
         return result
 
+
 print(mysum_bigger_than(10, 5, 20, 30, 6))
 print(mysum_bigger_than('k', 'I', 'am', 'interested', 'in', 'learning', 'Python'))
+
+"""
+Write a function, sum_numeric, that takes any number of arguments. If the argument is or can be 
+turned int an integer, then it should be added to the total. Arguments that can't be handled as 
+integers should be ignored. The result is the sum of the numbers. 
+Thus, sum_numeric(10, 20, 'a', '30', 'bcd') would return 60. 
+"""
+
+
+def sum_numeric(*args):
+    num_sum = []
+    for item in args:
+        try:
+            num_sum.append(int(item))
+        except ValueError:
+            continue
+    return sum(num_sum)
+
+
+print(sum_numeric(10, 20, 'a', '30', 'bcd'))
+
+
