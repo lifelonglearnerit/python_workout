c120b9065c3a639298065334eb0eb1a576ae34ec
lifelonglearnerit <lifelonglearnerit@gmail.com>
2022-06-26 11:43:05 +0200

    exercise 16 - added new function and test function

--- exercise_16_dictdiff.py
+++ exercise_16_dictdiff.py
@@ -6,16 +6,41 @@ each key-value pair that differs, the return value of dictdiff will have a key-v
 in which the value is a list containing the values from the two different dicts. If one of
 the dicts doesn't contain that key (my note: i think it should be value), it should contain None.
 """
-d1 = {'a': 1, 'b': 2, 'c': 3}
-d2 = {'a': 1, 'b': 2, 'c': 4}
-d3 = {'a': 1, 'b': 2, 'd': 3}
-d4 = {'a': 1, 'b': 2, 'c': 4}
-d5 = {'a': 1, 'b': 2, 'd': 4}
+from typing import Callable
+from typing import Dict
+
+DICTS = {
+    'd1': {'a': 1, 'b': 2, 'c': 3},
+    'd2': {'a': 1, 'b': 2, 'c': 4},
+    'd3': {'a': 1, 'b': 2, 'd': 3},
+    'd4': {'a': 1, 'b': 2, 'c': 4},
+    'd5': {'a': 1, 'b': 2, 'd': 4}
+}
+
+TESTS = {
+    ('d1', 'd1'): {},
+    ('d1', 'd2'): {'c': [3, 4]},
+    ('d3', 'd4'): {'c': [None, 4], 'd': [3, None]},
+    ('d1', 'd5'): {'c': [3, None], 'd': [None, 4]}
+}
+
+
+def test(f: Callable[[dict, dict], dict], f_param: tuple, tests: dict) -> bool:
+    """
+    Function for testing functions for exercise 16 from
+    Python Workout book.
+    :param f: function comparing 2 dictionaries
+    :param f_param: parameters of function f
+    :param tests: dictionary of test cases
+    :return: bool value where True: Test passed; False: Test failed
+    """
+    print(f'Test: {f_param}\nExpected result: {tests[f_param]}\nResult: {f}\nPassed: ')
+    return True if f == tests[f_param] else False
 
 
 # first solution - for dicts of equal length
 # naive solution
-def dictdiff(dict_1: dict, dict_2: dict) -> dict:
+def dictdiff1(dict_1: dict, dict_2: dict) -> dict:
     kv_dict_1 = dict_1.items()
     kv_dict_2 = dict_2.items()
     diffdict = dict()
@@ -26,19 +51,12 @@ def dictdiff(dict_1: dict, dict_2: dict) -> dict:
             diffdict[items_1[0]] = [dict_1.get(items_1[0]), dict_2.get(items_1[0])]
         elif items_1[0] != items_2[0]:
             diffdict[items_1[0]] = [dict_1.get(items_1[0]), dict_1.get(items_2[0])]
-            diffdict[items_2[0]] = [dict_1.get(items_2[0]), dict_2.get(items_2[0]),]
-
+            diffdict[items_2[0]] = [dict_1.get(items_2[0]), dict_2.get(items_2[0]), ]
     return diffdict
 
-print(dictdiff(d1, d1))
-print(dictdiff(d1, d2))
-print(dictdiff(d3, d4))
-print(dictdiff(d1, d5))
 
-
-def diffdict(dict_1: dict, dict_2: dict) -> dict:
+def diffdict2(dict_1: dict, dict_2: dict) -> dict:
     diff = dict()
-    diff_list = list()
     if dict_1 == dict_2:
         return diff
     else:
@@ -51,24 +69,32 @@ def diffdict(dict_1: dict, dict_2: dict) -> dict:
 
     return diff
 
-print(diffdict(d1, d1))
-print(diffdict(d1, d2))
-print(diffdict(d3, d4))
-print(diffdict(d1, d5))
+
+# solution after getting hint about using set .union() to gather keys that
+# are in both sets
+def diffdict3(dict_1: dict, dict_2: dict) -> dict:
+    diff = dict()
+    common_keys = dict_1.keys() | dict_2.keys()
+    for key in common_keys:
+        if dict_1.get(key) != dict_2.get(key):
+            diff[key] = [dict_1.get(key),
+                         dict_2.get(key)]
+    return diff
+
+# -------------------------------------------TESTING-------------------------------------------------
 
 
+FUNCTIONS = {
+    1: dictdiff1,
+    2: diffdict2,
+    3: diffdict3
+}
 
-"""
-Planning notes for solution 1: 
-What I want to do:
-- compare both keys and values and check if there is a difference
-- if there is a difference - save it in dict 
-- difference is stored as key = key that we are comparing
-- if under this index there are 2 different keys, then store 2 different dicts 
-- values = list(value_dict_1, value_dict_2) 
-- if there is no difference - return empty dict 
+def run_all():
+    for key in TESTS:
+        for function in FUNCTIONS.values():
+            print(f'----------------------{function}----------------------')
+            print(test(function(DICTS[key[0]], DICTS[key[1]]), (key[0], key[1]), TESTS))
 
-What I need:
-- key values pairs - items() and get()
-- for loop or while loop - both solutions :)?
-"""
+run_all()
+
