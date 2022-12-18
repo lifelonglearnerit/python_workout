fcca4c93eb8effe3bb5e1eaf085c9a45d346d7bb
lifelonglearnerit <lifelonglearnerit@gmail.com>
2022-12-18 13:33:52 +0100

    exercise 18 - find_integers_in_file function added

--- exercise_18_final_line.py
+++ exercise_18_final_line.py
@@ -31,3 +31,22 @@ def get_final_line_2(file_name: str) -> None:
 
 
 print(get_final_line_2('exercise_18_file.txt'))
+
+# Beyond the excercise
+"""Iterate over the lines of a text file. Find all of the words (i.e., non-whitespace surrounded by whitespace) 
+that contain only integers, and sum them"""
+# I will use split() since it uses white space by default and is digit to confirm i is a digit
+def find_integers_in_file(file_name: str) -> None:
+    with open(file_name) as f:
+        words = f.read().split()
+        return sum(int(x) for x in words if x.isdigit())
+
+print(find_integers_in_file('exercise_18_file.txt'))
+
+# modification to include also numbers surrounded with ","
+def find_integers_in_file_2(file_name: str) -> None:
+    with open(file_name) as f:
+        words = f.read().split()
+        return sum(int(x.strip(",")) for x in words if x.strip(",").isdigit())
+
+print(find_integers_in_file_2('exercise_18_file.txt'))
